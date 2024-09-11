f310ba2fbf457736ad50df3d211a77a3f805268f
Aravind <aravindmunna240@gmail.com>
2024-09-11 16:04:27 +0530

    Stream : Find the number of elements in the list

--- src/medium/FindFirstElement.java
+++ src/medium/FindFirstElement.java
@@ -10,10 +10,16 @@ public class FindFirstElement {
 
         List<Integer> integerList = Arrays.asList(11,2,4,5,6,4,3,22,2,3);
         System.out.println("First element is "+ findFirstElement(integerList).get());
+        System.out.println("Count of elements is  "+ findCountOfElements(integerList));
     }
 
     private static Optional<Integer> findFirstElement(List<Integer> integerList){
         return integerList.stream()
                 .findFirst();
+    }
+
+    private static long findCountOfElements(List<Integer> integerList){
+
+        return integerList.size();
     }
 }
